ef24917db7570d6fe2b28edd2df51d544f8836a5
sumeragizzz <forester@cocoa.plala.or.jp>
2016-11-06 00:09:58 +0900

    コールバックの実装を見直し

--- src/main/java/examples/api/messaging/line/resource/CallbackResource.java
+++ src/main/java/examples/api/messaging/line/resource/CallbackResource.java
@@ -1,6 +1,9 @@
 package examples.api.messaging.line.resource;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
@@ -9,14 +12,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.linecorp.bot.client.LineMessagingServiceBuilder;
 import com.linecorp.bot.client.LineSignatureValidator;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.CallbackRequest;
+import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.MessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.response.BotApiResponse;
@@ -29,11 +31,8 @@ public class CallbackResource {
     private static final String CHANNEL_SECRET = "CHANNEL_SECRET";
     private static final String CHANNEL_ACCESS_TOKEN = "CHANNEL_ACCESS_TOKEN";
 
-    private static final Logger logger = LoggerFactory.getLogger(CallbackResource.class);
-
     @POST
     @Consumes("application/json")
-    @SuppressWarnings("unchecked")
     public Response post(@Context HttpServletRequest req) {
         String channelSecret = System.getenv(CHANNEL_SECRET);
         String channelToken = System.getenv(CHANNEL_ACCESS_TOKEN);
@@ -42,19 +41,22 @@ public class CallbackResource {
                 new LineSignatureValidator(channelSecret.getBytes()));
         try {
             CallbackRequest callbackRequest = lineBotCallbackRequestParser.handle(req);
-            logger.trace(callbackRequest.toString());
-            callbackRequest
-                    .getEvents()
-                    .stream()
-                    .filter(t -> t instanceof MessageEvent && MessageEvent.class.cast(t).getMessage() instanceof TextMessageContent)
-                    .map(t -> (MessageEvent<TextMessageContent>) t)
-                    .findFirst()
-                    .ifPresent(t -> reply(channelToken, t.getReplyToken(), t.getMessage().getText()));
-
+            for (MessageEvent<?> messageEvent : filterEvents(callbackRequest.getEvents(), MessageEvent.class)) {
+                filterMessageContent(messageEvent.getMessage(), TextMessageContent.class)
+                        .ifPresent(t -> reply(channelToken, messageEvent.getReplyToken(), t.getText()));
+            }
         } catch (LineBotCallbackException | IOException e) {
             e.printStackTrace();
         }
         return Response.ok().build();
+    }
+
+    private <T extends Event> List<T> filterEvents(List<Event> events, Class<T> clazz) {
+        return events.stream().filter(t -> clazz.isAssignableFrom(t.getClass())).map(t -> clazz.cast(t)).collect(Collectors.toList());
+    }
+
+    private <T extends MessageContent> Optional<T> filterMessageContent(MessageContent messageContent, Class<T> clazz) {
+        return Optional.ofNullable(messageContent).filter(t -> clazz.isAssignableFrom(t.getClass())).map(t -> clazz.cast(t));
     }
 
     private void reply(String channelToken, String replyToken, String textMessage) {
